e5eb843ad0db33c8ea9a78eb182390e36266ac4e
Misbah Aamer <misbahuddin.aamer@gmail.com>
2021-05-06 23:24:07 -0400

    dynamic checkboxes

---
+++ Models/Persons.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorWebApp.Models
+{
+    public class Persons
+    {
+        public int PersonId { get; set; }
+        public string PersonName { get; set; }
+    }
+    public class Subscriptions
+    {
+        public int SubcriptionId { get; set; }
+        public string SubscriptionName { get; set; }
+    }
+    public class PerSubs
+    {
+        public int PersonId { get; set; }
+        public string PersonName { get; set; }
+        public bool Amazon { get; set; }
+        public bool Netflix { get; set; }
+        public bool Hulu { get; set; }
+        public bool Peacock { get; set; }
+        public bool Disney { get; set; }
+    }
+    public class PersonSubs
+    {
+
+        public List<PerSubs> plist = new List<PerSubs>()
+        { 
+            new PerSubs()
+            {
+                PersonId = 1,
+                PersonName = "Person1",
+                Amazon = false,
+                Netflix = false,
+                Hulu = false,
+                Peacock = false,
+                Disney = false
+                
+            },
+            new PerSubs()
+            {
+                PersonId = 2,
+                PersonName = "Person2",
+                Amazon = false,
+                Netflix = false,
+                Hulu = false,
+                Peacock = false,
+                Disney = false
+
+            },
+            new PerSubs()
+            {
+                PersonId = 3,
+                PersonName = "Person3",
+                Amazon = false,
+                Netflix = false,
+                Hulu = false,
+                Peacock = false,
+                Disney = false
+            },
+            new PerSubs()
+            {
+                PersonId = 4,
+                PersonName = "Person4",
+                Amazon = false,
+                Netflix = false,
+                Hulu = false,
+                Peacock = false,
+                Disney = false
+            },
+            new PerSubs()
+            {
+                PersonId = 5,
+                PersonName = "Person5",
+                Amazon = false,
+                Netflix = false,
+                Hulu = false,
+                Peacock = false,
+                Disney = false
+            }
+        };
+       
+
+    }
+
+}
--- Pages/Public/Confirm.cshtml.cs
+++ Pages/Public/Confirm.cshtml.cs
@@ -15,13 +15,70 @@ namespace RazorWebApp.Pages.Public
         public string firstname { get; set; }
         public string lastname { get; set; }
 
-        public Form formdata { get; set; }
+        [BindProperty]
+        public PersonSubs personSubs { get; set; }
+        [BindProperty]
+        public List<Subscriptions> subsList { get; set; }
+        [BindProperty]
+        public List<string> Amazon { get; set; }
+        [BindProperty]
+        public List<string> Netflix { get; set; }
+        [BindProperty]
+        public List<string> Hulu { get; set; }
+        [BindProperty]
+        public List<string> Peacock { get; set; }
+        [BindProperty]
+        public List<string> Disney { get; set; }
+
 
         public void OnGet()
         {
-            firstname = HttpContext.Session.GetString("firstname");
-            lastname = SessionHelper.GetObjectFromJson<string>(HttpContext.Session, "lastname");
+            //firstname = HttpContext.Session.GetString("firstname");
+            //lastname = SessionHelper.GetObjectFromJson<string>(HttpContext.Session, "lastname");
+            PersonSubs ps = new PersonSubs();
+            
+            personSubs = ps;
+            var slist = LoadSubscription();
+            subsList = slist;
         }
-       
+
+        private List<Subscriptions> LoadSubscription()
+        {
+            var SubscriptionList = new List<Subscriptions>()
+                { new Subscriptions()
+                    {
+                        SubcriptionId = 1,
+                        SubscriptionName = "Amazon"
+                    },
+                    new Subscriptions()
+                    {
+                        SubcriptionId = 2,
+                        SubscriptionName = "Hulu"
+                    },
+                    new Subscriptions()
+                    {
+                        SubcriptionId = 3,
+                        SubscriptionName = "Disney"
+                    },
+                    new Subscriptions()
+                    {
+                        SubcriptionId = 4,
+                        SubscriptionName = "Peacock"
+                    },
+                    new Subscriptions()
+                    {
+                        SubcriptionId = 5,
+                        SubscriptionName = "Netflix"
+                    }};
+            return SubscriptionList;
+        }
+        public void OnPostSaveChanges(PersonSubs personSubs)
+        {
+            var amazon = Amazon    ;
+            var netflix = Netflix   ;
+            var hulu = Hulu      ;
+            var peacock = Peacock   ;
+            var  disney =    Disney;
+    }
     }
 }
